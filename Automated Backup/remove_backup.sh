d7b0babcb26d4f817bee4dfd021853b7c05e2fee
SHUBHAM RAWAT <77045126+rawatshubham138@users.noreply.github.com>
2023-03-29 23:22:04 +0530

    Update remove_backup.sh

--- Automated Backup/remove_backup.sh
+++ Automated Backup/remove_backup.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+#########################
+# Author: Shubham Rawat 
+# Date: 25/03/2023
+#
+# Version: v1
+#
+# This script will automatically delete backup after 7 days
+#########################
+
+
 find "/backupfolder" -type f -mtime +7 -exec rm {} \;
